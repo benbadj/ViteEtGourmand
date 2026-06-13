3ad6111135377ee5a52067e99d9908e1878959fa
benbadj <benbadj0033@gmail.com>
2026-06-13 12:13:12 +0200

    navbar and home

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("","menu","/pages/menu.html")
+    
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite et Gourmand";
