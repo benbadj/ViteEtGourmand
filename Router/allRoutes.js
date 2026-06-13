00b827996dabe4d2d4ef70b8480c6afbd8c26f41
benbadj <benbadj0033@gmail.com>
2026-06-13 16:48:14 +0200

    ajout pages html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("","menu","/pages/menu.html")
-    
+    new Route("/menu","menu","/pages/menu.html"),
+    new Route("/contact","contact","/pages/contact.html"),
+    new Route("/signup","S'inscrire","/pages/signup.html"),
+    new Route("/auth","connexion","/pages/auth.html"),
+    new Route("/password","Mot de passe","/pages/auth.html"),  
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
